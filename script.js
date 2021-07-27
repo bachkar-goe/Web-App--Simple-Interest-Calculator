59208cb3a2b2990943838cf8ba8f15d3320afe4f
bachkar-goe <79193513+bachkar-goe@users.noreply.github.com>
2021-07-27 16:09:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function compute()
 {
-    document.getElementById("result").innerHTML = "";
+	document.getElementById("result").innerHTML = "";
 
 	var principal = document.getElementById("principal").value;
   
-    if (principal === "")
-        {alert('!! Empty Amount !! .. This have be a number');
-	    document.getElementById("principal").focus();
-    } else if (principal == 0)
+  if (principal === "")
+     {alert('!! Empty Amount !! .. This have be a number');
+	document.getElementById("principal").focus();
+  } else if (principal == 0)
 
-	    {alert(' !! Zero Amount Number !! .. This have be greater than zero ');
-	    document.getElementById("principal").focus();
-        } else if (principal < 0)
-        {alert(' !! Negative Amount Number !! .. This have to be greater than zero');
-    document.getElementById("principal").focus();}
-    else {
-    
-    var principal = document.getElementById("principal").value;
+	{alert(' !! Zero Amount Number !! .. This have be greater than zero ');
+	document.getElementById("principal").focus();
+
+} else if (principal < 0)
+{alert(' !! Negative Amount Number !! .. This have to be greater than zero');
+document.getElementById("principal").focus();}
+else {
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-document.getElementById("result").innerHTML="If you deposit <mark>" +principal+ ",</mark> \<br\>at an interest rate of <mark>"+rate+"% </mark> \<br\>You will receive an amount of <mark> "+interest+",</mark> \<br\>in the year <mark> "+year+" </mark>\<br\>"}
+document.getElementById("result").innerHTML="If you deposit <mark>" +principal+ ",</mark> \<br\>at an interest rate of <mark>"+rate+"% </mark> \<br\>You will receive an amount of <mark> "+interest+",</mark> \<br\>in the year <mark> "+year+" </mark>\<br\>"}}
+
+ 
 function updateRate() 
     {   
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
-    }        
-        
+    }      
